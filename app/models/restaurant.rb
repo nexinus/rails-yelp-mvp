431b83d9d1cf6199351dacfd59cb5df1f2c0104f
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-08 17:15:47 +0200

    Seed added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
   # A restaurant must have at least a name, an address and a category.
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :name, :address, presence: `true`
+  # validates :category, presence: true, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
+  validates :category, inclusion: { in: CATEGORIES }
 end
